a68764369a1f6cded91e68ecacb8fdd5bd3ffaa9
Louis <louis.zhang@mintinglabs.com>
2025-03-26 00:52:56 +0800

    first commit

---
+++ deepseek.py
@@ -0,0 +1,19 @@
+# Please install OpenAI SDK first: `pip3 install openai`
+from openai import OpenAI
+from langchain_community.tools import DuckDuckGoSearchRun
+
+client = OpenAI(api_key="sk-a6ca73fb7c7b4f549d9ff068f37b9b26", base_url="https://api.deepseek.com")
+
+
+#  实现deepseek api调用
+def chat(prompt: str, system_prompt=None):
+    response = client.chat.completions.create(
+        model="deepseek-reasoner",
+        messages=[
+            {"role": "system", "content": system_prompt},
+            {"role": "user", "content": prompt},
+        ],
+        stream=False
+    )
+
+    return response.choices[0].message.content
